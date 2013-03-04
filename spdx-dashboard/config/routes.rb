54a78db5bc034caf07e6e8a4a22b20fae933c3d4
Mike Santo <mike.j.santo@gmail.com>
2013-03-04 14:43:37 -0600

    changed root action to SpdxDocsController#index

--- spdx-dashboard/config/routes.rb
+++ spdx-dashboard/config/routes.rb
@@ -1,7 +1,7 @@
 SpdxDashboard::Application.routes.draw do
   resources :spdx_docs
 
-
+  root to: "spdx_docs#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
