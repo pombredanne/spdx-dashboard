fcbc0ecc85630b9cfe00befd915d12c94eeb7523
Mike Santo <mike.j.santo@gmail.com>
2013-03-24 02:33:31 -0500

    more comments stuff

--- app/app/controllers/comments_controller.rb
+++ app/app/controllers/comments_controller.rb
@@ -1,20 +1,22 @@
 class CommentsController < ApplicationController
+  before_filter :get_owner
+
+  def index
+    @comments = @owner.comments
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @comments }
+    end
+  end
 
   def create
-    if params[:package_id].present?
-      @owner = Package.find(params[:package_id])
-    elsif params[:spdx_doc_id].present?
-      @owner = SpdxDoc.find(params[:spdx_doc_id])
-    elsif params[:package_file_id].present?
-      @owner = PackageFile.find(params[:package_file_id])
-    end
-
     @comment = Comment.new(params[:comment])
 
     @comment.owner = @owner
 
     respond_to do |format|
-      redirect = @owner.class == "SpdxDoc" ? @owner : @owner.spdx_doc
+      redirect = @owner.is_a?(SpdxDoc) ? @owner : @owner.spdx_doc
       if @comment.save
         format.html { redirect_to redirect, notice: 'Your comment was added successfully.' }
         format.json { render json: @comment, status: :created, location: redirect }
@@ -22,6 +24,17 @@ class CommentsController < ApplicationController
         format.html { redirect_to redirect }
         format.json { render json: @comment, status: :unprocessable_entity }
       end
+    end
+  end
+
+  private
+  def get_owner
+    if params[:package_id].present?
+      @owner = Package.find(params[:package_id])
+    elsif params[:spdx_doc_id].present?
+      @owner = SpdxDoc.find(params[:spdx_doc_id])
+    elsif params[:package_file_id].present?
+      @owner = PackageFile.find(params[:package_file_id])
     end
   end
 end
