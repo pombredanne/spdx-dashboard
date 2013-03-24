92f00ce3ac1ee04b52defd9376b87d8f78cf2398
Mike Santo <mike.j.santo@gmail.com>
2013-03-23 20:39:21 -0500

    comment model

---
+++ app/app/models/comment.rb
@@ -0,0 +1,3 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :content, :creator_id, :owner_id, :owner_type
+end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324004040) do
+ActiveRecord::Schema.define(:version => 20130324013645) do
 
   create_table "checksums", :force => true do |t|
     t.integer  "owner_id"
     t.string   "owner_type"
     t.string   "algorithm"
     t.string   "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.text     "content"
+    t.integer  "creator_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
