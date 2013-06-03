1a59a8c01db4a587344e5bed825fe2cbf18a74e0
Mike Santo <mike.j.santo@gmail.com>
2013-06-03 02:08:10 -0500

    format attr_accessible on package.rb

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,5 +1,17 @@
 class Package < ActiveRecord::Base
-  attr_accessible :checksum_id, :copyright, :description, :download_location, :filename, :name, :originator, :spdx_doc_id, :summary, :supplier, :version
+  attr_accessible(
+    :checksum_id, 
+    :copyright, 
+    :description, 
+    :download_location, 
+    :filename, 
+    :name, 
+    :originator, 
+    :spdx_doc_id, 
+    :summary, 
+    :supplier, 
+    :version)
+
   belongs_to :spdx_doc
   has_one :checksum, as: :owner
   has_many :optional_fields, as: :owner
